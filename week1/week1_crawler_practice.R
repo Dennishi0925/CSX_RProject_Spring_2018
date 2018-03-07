14192386dfceecccba501405ca54ac5ab57e14d3
Dennishi0925 <tsengmoneymoney@gmail.com>
2018-03-07 22:57:34 +0800

    upload hw1

---
+++ week1/week1_crawler_practice.R
@@ -0,0 +1,31 @@
+library(rvest)
+library(stringr)
+rm(list = ls())
+url = "https://www.datacamp.com/courses/all"
+webpage <- read_html(url, encoding = "UTF-8")
+
+# check items
+str_extract_all(webpage, pattern = ".course-block__\\w+\\w*")
+table(str_extract_all(webpage, pattern = ".course-block__\\w+\\w*"))
+
+# writing functions
+crawl_datacamp <- function(x) {
+y <- html_nodes(webpage, str_c('.course-block__', x))
+z <- html_text(y)
+}
+
+###
+title <- crawl_datacamp("title")
+author <- crawl_datacamp("author")
+description <- crawl_datacamp("description")
+description <- str_replace_all(description, "\\n[:blank:]+", "")
+length  <- crawl_datacamp("length ")
+author <- crawl_datacamp("author")
+# language
+
+str_extract_all(webpage, pattern = ".course-block__technology[:graph:]*")
+language <- unlist(str_extract_all(webpage, pattern = ".course-block__technology--\\w*"))
+language <- str_replace_all(language, pattern = ".course-block__technology--", replacement = "")
+
+datacamp <- data.frame(title, author, description, length, language)
+View(datacamp)
