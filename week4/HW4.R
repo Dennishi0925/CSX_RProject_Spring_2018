c948bc25e8defdcd109ccbdde8ebf51358bf32d9
Dennishi0925 <tsengmoneymoney@gmail.com>
2018-04-14 01:24:41 +0800

    utf8

--- week4/HW4.R
+++ week4/HW4.R
@@ -1,6 +1,7 @@
 library(jsonlite)
 library(tidyverse)
 library(wordcloud2)
+options(Encoding="UTF-8")
 rm(list = ls())
 data <- fromJSON("http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=a8fd6811-ab29-40dd-9b92-383e8ebd2a4e") 
 str(data)
@@ -8,6 +9,6 @@ df <- data$result$results
 str(df)
 View(df)
 ci <- df$CITYZONE %>% na.omit() %>%
-  str_replace_all("∞œ", "")
+  str_replace_all("ÂçÄ", "")
 tci <- table(ci)
 wordcloud2(tci)
